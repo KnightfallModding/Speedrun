28f85b127266503de5649fcf3d446ae39589bc78
t1nquen <quentin.sereno3@gmail.com>
2025-06-05 14:41:42 +0200

    [~] minor code syntax fixes

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,6 @@
 ﻿using Speedrun;
 using UnityEngine;
 using MelonLoader;
-using Il2CppInterop.Runtime.Injection;
 
 [assembly: MelonInfo(typeof(Plugin), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR, $"{ModInfo.MOD_LINK}/releases/latest/download/Release.zip")]
 [assembly: MelonGame("Landfall Games", "Knightfall")]
@@ -24,31 +23,18 @@ internal class Plugin : MelonMod
         }
 
         LoggerInstance.Msg($"Plugin {ModInfo.MOD_NAME} loaded successfully!");
-
-        RegisterComponents();
     }
 
+    /// <summary>
+    /// Load or create all MelonLoader config entries.
+    /// Stored in `UserData/MelonPreferences.cfg`.
+    /// </summary>
     private void InitConfig()
     {
         var mlCategory = MelonPreferences.CreateCategory("Speedrun", "Speedrun settings");
 
         ENABLED = mlCategory.CreateEntry("Enabled", true);
         MAP_KEY = mlCategory.CreateEntry("ToggleMapKey", KeyCode.M);
-    }
-
-    /// <summary>
-    /// Register custom classes to Il2Cpp.
-    /// </summary>
-    private static void RegisterComponents()
-    {
-        if (!ClassInjector.IsTypeRegisteredInIl2Cpp(typeof(SpawnMap)))
-            ClassInjector.RegisterTypeInIl2Cpp(typeof(SpawnMap));
-
-        if (!ClassInjector.IsTypeRegisteredInIl2Cpp(typeof(ShowRecords)))
-            ClassInjector.RegisterTypeInIl2Cpp(typeof(ShowRecords));
-
-        if (!ClassInjector.IsTypeRegisteredInIl2Cpp(typeof(Timer)))
-            ClassInjector.RegisterTypeInIl2Cpp(typeof(Timer));
     }
 
     /// <summary>
--- src/Components/Timer.cs
+++ src/Components/Timer.cs
@@ -19,33 +19,33 @@ public class Timer : MonoBehaviour
         timerText = this.GetComponent<TextMeshProUGUI>();
     }
 
-    private void Update() {
-        if (hasStarted) {
+    private void Update()
+    {
+        if (hasStarted)
             timer += Time.deltaTime;
-        }
 
-        #if DEBUG
-        if (Input.GetKeyDown(KeyCode.F10)) {
+#if DEBUG
+        if (Input.GetKeyDown(KeyCode.F10))
             timer += 10f;
-        }
-        if (Input.GetKeyDown(KeyCode.N)) {
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
             GameObject horse = Player.localplayer.data.belovedHorse.gameObject;
             horse.transform.position = new Vector3(-11f, 256.9f, 2200f); // Castle
         }
 
-        if (Input.GetKeyDown(KeyCode.B)) {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
             GameObject horse = Player.localplayer.data.belovedHorse.gameObject;
             horse.transform.position = transform.position = new Vector3(-523f, 156f, 580f); // Court
         }
-        #endif
+#endif
     }
 
     private void OnGUI()
     {
         if (timerText != null)
-        {
             timerText.text = GetTimerFullText();
-        }
     }
 
     public string GetTimerFullText()
@@ -53,10 +53,10 @@ public class Timer : MonoBehaviour
         // This is required to display the 'expected' spawn point to the user.
         // This corresponds to the actual numbers shown on the SpeedrunMap.
         int spawn = SpawnHandler.shownSpawnPoint;
-        float record = RecordsHandler.GetRecord(spawn-1);
+        float record = RecordsHandler.GetRecord(spawn - 1);
 
         string formattedBest = record != -1 ? GetFormattedTime(record) : "N/A";
-        
+
         // Change color based on time comparison with records
         bool isRecord = RecordsHandler.IsRecord(spawn - 1, timer);
 
@@ -73,24 +73,17 @@ public class Timer : MonoBehaviour
         return text;
     }
 
-    public void StopTimer()
-    {
-        hasStarted = false;
-    }
+    public void StopTimer() => hasStarted = false;
+    public float GetTime() => this.timer;
+    public string GetFormattedTime() => GetFormattedTime(this.timer);
 
-    public float GetTime()
-    {
-        return this.timer;
-    }
-
-    public string GetFormattedTime()
-    {
-        return GetFormattedTime(this.timer);
-    }
-
+    /// <summary>
+    /// Format float timer to "00:00.00"
+    /// </summary>
+    /// <param name="_timer"></param>
+    /// <returns></returns>
     public static string GetFormattedTime(float _timer)
     {
-        // Format timer to 00:00.00
         int minutes = Mathf.FloorToInt(_timer / 60);
         int seconds = Mathf.FloorToInt(_timer % 60);
         int milliseconds = Mathf.FloorToInt((_timer * 100) % 100);
